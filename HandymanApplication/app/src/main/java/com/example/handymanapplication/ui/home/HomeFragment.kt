3c5ae8410996ab6471e6548b16fd6fa1c40b352e
ahmad <ahmadeldorra1@gmail.com>
2020-01-25 20:42:59 +0200

    Hamada il dabe3 testing edit Profile API (method:get)

--- HandymanApplication/app/src/main/java/com/example/handymanapplication/ui/home/HomeFragment.kt
+++ HandymanApplication/app/src/main/java/com/example/handymanapplication/ui/home/HomeFragment.kt
@@ -1,22 +1,34 @@
 package com.example.handymanapplication.ui.home
 
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.handymanapplication.R
 import com.example.handymanapplication.adapters.HomeAdapter
+
 import kotlinx.android.synthetic.main.fragment_home.*
 import org.json.JSONObject
+import android.widget.Toast
+import com.example.handymanapplication.Utils.Constants
+import com.example.handymanapplication.Utils.SharedPreferences
+import com.example.handymanapplication.Utils.Utils
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.json.responseJson
+import com.github.kittinunf.result.failure
+import com.github.kittinunf.result.success
 
 class HomeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
         return  inflater.inflate(R.layout.fragment_home, container, false)
     }
 
@@ -36,7 +48,36 @@ class HomeFragment : Fragment() {
         homeAdapter.setItem(JSONObject().apply {
             put("name", "Mohammad")
         })
-
-
     }
+   fun ViewProfile(){
+       Fuel.get(
+           Utils.API_PROFILE
+                   ,listOf()
+       )
+           .header("accept" to "application/json")
+           .responseJson { _, _, result ->
+               result.success {
+                   var res = it.obj()
+                   if(res.optString("status","error")== "sucess"){
+                       var profile = res.getJSONObject("profile")
+
+                        activity?.runOnUiThread {
+                            Toast.makeText(activity,profile.toString(),Toast.LENGTH_LONG).show()
+
+                        }
+                   }
+                   else{
+
+                   }
+
+               }
+               result.failure {
+
+               }
+           }
+       )
+
+   }
+
+
 }
